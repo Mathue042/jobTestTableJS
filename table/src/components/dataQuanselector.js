fa89d25c9e7fa8d562b6e6bf6915ee981a4ec6ce
matvey767 <matvey767@gmail.com>
2021-02-19 01:29:11 +0300

    осталось сделать еще немного

--- table/src/components/dataQuanselector.js
+++ table/src/components/dataQuanselector.js
@@ -1,16 +1,47 @@
 import react, {useState, useEffect} from 'react'
 import TableJS from './table'
-import {Button} from 'antd'
+import {Button, Input} from 'antd'
 
 const PickQuan = () => {
    
-const [state, setState] = useState({})
+  
+const [state, setState] = useState({
+    isTableReady: false
+})
+
+const updateData = (value) => {
+    setState({...state, isTableReady: value })
+ }
+
+function html(){
+    if(state.isTableReady === true){
+    return (
+        <div>
+        <Input enterButton='Искать' size = 'large' style = {{width: 300}} placeholder="Поиск по таблице"/>
+        <Button size = 'large' onClick={()=>{setState({...state, isLarge: true})}}>большая таблица</Button>
+        <Button size = 'large' onClick={()=>{setState({...state, isLarge: false})}}>маленькая таблица</Button>
+        <Button size = 'large'>добавить строку</Button>
+      
+        {state.isLarge === undefined ? <></> : <TableJS isLarge ={state.isLarge} isLoading ={true} callback = {updateData}/>}
+        </div>
+    )
+    }else{
+        return (
+            <div>
+            <Button size = 'large' onClick={()=>{setState({...state, isLarge: true})}}>большая таблица</Button>
+            <Button size = 'large' onClick={()=>{setState({...state, isLarge: false})}}>маленькая таблица</Button>
+            {state.isLarge === undefined ? <></> : <TableJS isLarge ={state.isLarge} isLoading ={true} callback = {updateData}/>}
+            </div>
+        )
+    }
+}
+
+
+
     return(
-    <div style = {{textAlign: 'center'}}>
-    <Button size = 'large' onClick={()=>{setState({...state, isLarge: true})}}>большая таблица</Button>
-    <Button size = 'large' onClick={()=>{setState({...state, isLarge: false})}}>маленькая таблица</Button>
-    {state.isLarge === undefined ? <></> : <TableJS isLarge ={state.isLarge} isLoading ={true}/>}
-    </div>
+    <header style = {{textAlign: 'center'}}>
+        {html()}
+    </header>
     )
 }
 
--- table/src/components/table.js
+++ table/src/components/table.js
@@ -12,60 +12,77 @@ const TableJS = (props) => {
         {
           title: 'id',
           dataIndex: 'id',
+          defaultSortOrder: 'ascend',
+    sorter: (a, b) => a.id - b.id,
+    sortDirections: ['descend', 'ascend']
         },
         {
           title: 'firstName',
           dataIndex: 'firstName',
+          onFilter: (value, record) => record.firstName.indexOf(value) === 0,
+          sorter: (a, b) => a.firstName.length - b.firstName.length,
+          sortDirections: ['descend', 'ascend']
         },
         {
           title: 'lastName',
           dataIndex: 'lastName',
+          onFilter: (value, record) => record.lastName.indexOf(value) === 0,
+          sorter: (a, b) => a.lastName.length - b.lastName.length,
+          sortDirections: ['descend', 'ascend'],
         },
         {
             title: 'email',
             dataIndex: 'email',
+            onFilter: (value, record) => record.email.indexOf(value) === 0,
+          sorter: (a, b) => a.email.length - b.email.length,
+          sortDirections: ['descend', 'ascend'],
         },
         {
             title: 'phone',
             dataIndex: 'phone',
+            sorter: (a, b) => a.id - b.id,
+            sortDirections: ['descend', 'ascend']
         },
         {
             title: 'streetAddress',
             dataIndex: 'streetAddress',
+            onFilter: (value, record) => record.streetAddress.indexOf(value) === 0,
+          sorter: (a, b) => a.streetAddress.length - b.streetAddress.length,
+          sortDirections: ['descend', 'ascend'],
         },
         {
             title: 'city',
             dataIndex: 'city',
+            onFilter: (value, record) => record.city.indexOf(value) === 0,
+          sorter: (a, b) => a.city.length - b.city.length,
+          sortDirections: ['descend', 'ascend'],
         },
         {
             title: 'state',
             dataIndex: 'State',
+            onFilter: (value, record) => record.State.indexOf(value) === 0,
+          sorter: (a, b) => a.State.length - b.State.length,
+          sortDirections: ['descend', 'ascend'],
         },
         {
             title: 'zip',
             dataIndex: 'zip',
+            sorter: (a, b) => a.id - b.id,
+    sortDirections: ['descend', 'ascend']
         },
         {
             title: 'description',
             dataIndex: 'description',
+            onFilter: (value, record) => record.description.indexOf(value) === 0,
+          sorter: (a, b) => a.description.length - b.description.length,
+          sortDirections: ['descend', 'ascend'],
         },
       ]
-    var dataSource = [{
-        id:'kek',
-        firstName : 'kek',
-        lastName : 'kek',
-        State : 'kek',
-        email : 'kek',
-        phone : 'kek',
-        city : 'kek',
-        streetAddress : 'kek',
-        zip : 'kek',
-        description : 'kek',
-    }]
-    
+    var dataSource = []
     useEffect(async () => {
         setState({...state, isLoading: true})
            const data = await getTableData(props.isLarge)
+           console.log(data);
             data.data.forEach(element => {
                dataSource.push({
                    id: element.id,
@@ -79,20 +96,16 @@ const TableJS = (props) => {
                    zip: element.address.zip,
                    description: element.description
                })
-           });      
-           console.log(dataSource)
-           console.log(state)
+           })
            setState((prevState)=>{
                return {...prevState, isLoading: false, dataSource: dataSource}
            })   
-           console.log(state);
+           props.callback(true)
     }, [props.isLarge])
 
-    
-    
     return(
         <div>
-           {state.isLoading === true ? <Spin/> : state.dataSource.length > 10? <Table columns = {columns} dataSource = {dataSource}/> : <></>}
+           {state.isLoading ==  true ? <Spin/> : <Table columns = {columns} dataSource = {state.dataSource}/>}
         </div>
     )
 }
